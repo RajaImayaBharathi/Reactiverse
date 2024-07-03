a240ddb8d2f9971267f1a6979cca6607fb02ef4e
Raja Imaya Bharathi <113758147+RajaImayaBharathi@users.noreply.github.com>
2024-07-03 23:51:47 +0530

    Create socialIoncs.jsx

---
+++ socialIoncs.jsx
@@ -0,0 +1,18 @@
+const Socialicons = () => {
+  return (
+    <div className="fixed bottom-0 left-0 flex flex-row text-white font-NavBar">
+      <a href="https://github.com" target="_blank" rel="noopener noreferrer">
+        linkedin .
+      </a>
+      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        GitHub .
+      </a>
+      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+        LeetCode
+      </a>
+      
+    </div>
+  )
+}
+
+export default Socialicons
