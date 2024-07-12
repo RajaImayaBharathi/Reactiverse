fc0edf2de2173e66f9af90ccd6148fc0458e1bc1
Raja Imaya Bharathi <113758147+RajaImayaBharathi@users.noreply.github.com>
2024-07-12 20:28:07 +0530

    Update SideBar.jsx

--- SideBar.jsx
+++ SideBar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { IoMailOpenOutline } from "react-icons/io5";
 const SideBar = () => {
+  /* const email = 'rajaimayabharathi@gmail.com'; */
+  /* const emailArray = email.split(''); */
+
   return (
     <React.Fragment>
-      <div className="flex items-center justify-end p-4 bg-gray-800 text-white ">
-        <div className="font-roboto flex items-center space-x-2 max-w-[20px]">
+      <div className="flex items-center justify-end p-4 text-white bg-maindark">
+        <div className="font-roboto flex items-center space-x-2 max-w-[20px] text-left inline-block transform rotate-90 space-between">
           <span className="text-left">rajaimayabharathi@gmail.com</span>
           <IoMailOpenOutline />
         </div>
-      </div>
+      </div> 
+      {/* <div className="flex flex-col items-center text-white font-roboto ">
+      {emailArray.map((char, index) => (
+        <span key={index} className="text-lg">
+          {char}
+        </span>
+      ))}
+    </div> */}
     </React.Fragment>
   );
 };
