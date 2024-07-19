9412c6af84eb274213b8a9423f9c9386cf467729
Raja Imaya Bharathi <113758147+RajaImayaBharathi@users.noreply.github.com>
2024-07-19 22:32:04 +0530

    Create ExperianceCard2.jsx

---
+++ ExperianceCard2.jsx
@@ -0,0 +1,102 @@
+<div className="m-10 flex flex-col lg:flex-row space-y-5">
+          <div className="space-y-5 mb-3 mr-10">
+            <div className="text-highlightOne" id="title">
+              <div>
+              <span className="font-Navbar">02</span>. Software Engineer{" "}
+                <span className="text-white">
+                  &ensp;.&ensp;{` ( July 2024 ~ Sep 2024)`}
+                </span>
+              </div>
+            </div>
+            <a
+              href="https://www.opmcorporation.com/"
+              target="_blank"
+              className="flex text-white items-center font-bold text-3xl space-x-4 "
+            >
+              <img src="public/OPMLogo.png" alt="" className="w-10 h-11 rounded-sm" />
+              <div>
+                <p>OPM Corporation</p>
+                <p className="text-sm font-normal">Bengaluru, India</p>
+              </div>
+              <GoArrowUpRight />
+            </a>
+            <div className="text-white max-w-[700px] companyInfo">
+              <p>
+                <span className="text-white">OPM Corporation</span> is a leading
+                business consulting firm founded in June 2020, dedicated to
+                providing tailored solutions and strategic guidance to
+                organizations across industries. With a team of seasoned
+                consultants, industry experts, and innovative thinkers.
+              </p>
+            </div>
+            <div className="text-white flex flex-wrap">
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                NEXT JS
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                React JS
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                Node JS
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                Express.js
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                Restful API
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                Postgres SQL
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                AWS
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                Git & Github
+              </p>
+              <p
+                className="mr-5 my-2 py-1 px-3 rounded-sm bg-gray-800 hover:bg-highlightOne text-white hover:text-zinc-900
+               text-sm"
+              >
+                JSON Web Tokens
+              </p>
+            </div>
+          </div>
+          <div className="p-10 rounded-lg bg-zinc-900 text-white space-y-4 lg:min-w-[500px] lg:max-w-[500px] experienceInfo">
+            <p>
+              As an <span>Software Developer Engineer Intern</span>, I developed
+              and enhanced the software by developing productive, responsive
+              components and robust backend for cross-functional browsers and
+              application support.
+            </p>
+            <p>
+              Focused on debugging and optimizing code to improve efficiency and
+              readability, and colloborating with frontend and backend sector to
+              implement enhancements aligned with business goals
+            </p>
+          </div>
+        </div>
